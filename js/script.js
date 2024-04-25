cb6d91ee4fb449804d40b4fe3653efa6565f75ae
Ali Amini <al935400@dal.ca>
2024-04-25 14:31:26 -0300

    created validation method for credentials

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*
+- Method for validating credentials
+- Method that sends you to next page on completion
+
+
+
+- password must be min 5 char long, 1 special char, 1 number, 1 capital
+*/
+
+
+let validation = ()=>{
+    let name = document.getElementById('email').value;
+    alert(name);
+}
